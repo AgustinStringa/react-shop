f503cd007642da18db79027b172c312c29110c69
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-17 16:57:31 -0300

    favicon

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "../ui/button/Button";
+import InputForm from "../ui/input-form/InputForm";
+import Logo from "../Logo/Logo";
+const Login = () => {
+  return (
+    <div>
+      <Logo height="50px" />
+      <h1 class="main-title">Create a new password</h1>
+      <p class="subtitle">Enter a new password for your account</p>
+
+      <form action="" class="sign-up-form">
+        <div class="input-field">
+          <label for="password">Password</label>
+          <InputForm type="password" placeholder="*********" />
+        </div>
+        <div class="input-field">
+          <label for="repeat-password">Re-enter password</label>
+          <InputForm type="password" placeholder="*********" />
+        </div>
+        <Button text="Confirm" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
