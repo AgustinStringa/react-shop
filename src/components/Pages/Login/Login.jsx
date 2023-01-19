911c4488bbb7b2803e70ac3ca77f27454204aaf7
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-19 10:59:55 -0300

    removing layout from login/signup

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import Button from "../../ui/button/Button";
 import InputForm from "../../ui/input-form/InputForm";
-import Logo from "../../Logo/Logo";
-import Layout from "../../Layout/Layout";
 import "../../App.scss";
 const Login = () => {
   return (
-    <Layout>
-      <div>
-        <form action="" className="sign-up-form">
-          <div className="input-field">
-            <label htmlFor="password">Email address</label>
-            <InputForm type="email" placeholder="camilayokoo@gmail.com" />
-          </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <InputForm type="password" placeholder="*********" />
-          </div>
-          <Button text="Log In" />
+    <div className="container-login">
+      <h1 className="main-title">Login</h1>
+      <form action="" className="sign-up-form">
+        <div className="input-field">
+          <label htmlFor="password">Email address</label>
+          <InputForm type="email" placeholder="camilayokoo@gmail.com" />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <InputForm type="password" placeholder="*********" />
+        </div>
+        <Button text="Log In" />
 
-          <p className="forgot-message">
-            <a href="/" className="form-link forgot-link">
-              Forgot my password
-            </a>
-          </p>
-        </form>
+        <p className="forgot-message">
+          <a href="/" className="form-link forgot-link">
+            Forgot my password
+          </a>
+        </p>
+      </form>
 
-        <Button text={"Sign up"} outline={true}></Button>
-      </div>
-    </Layout>
+      <Button text={"Sign up"} outline={true}></Button>
+    </div>
   );
 };
 
--- src/components/Pages/SignUp/SignUp.jsx
+++ src/components/Pages/SignUp/SignUp.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Button from "../../ui/button/Button";
 import InputForm from "../../ui/input-form/InputForm";
-import Logo from "../../Logo/Logo";
 import "./SignUp.css";
-import Layout from "../../Layout/Layout";
 import "../../App.scss";
 
 const SignUp = () => {
   return (
-    <Layout>
-      <div>
+    <>
+      <div className="container-signup">
         <h1 className="main-title">Create a new password</h1>
         <p className="subtitle">Enter a new password for your account</p>
 
@@ -25,7 +23,7 @@ const SignUp = () => {
           <Button text="Confirm" />
         </form>
       </div>
-    </Layout>
+    </>
   );
 };
 
