ffdc97c886c7f811d14c7175e4c32a7364425b22
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-19 16:54:13 -0300

    develop account

--- src/components/ui/input-form/InputForm.jsx
+++ src/components/ui/input-form/InputForm.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./InputForm.css";
-const InputForm = ({ type = "text", placeholder = "*****", id = "" }) => {
+const InputForm = ({
+  type = "text",
+  placeholder = "*****",
+  id = "",
+  disabled = false,
+}) => {
   return (
     <input
       className="input-form"
       type={type}
       name=""
       id={id}
+      disabled={disabled}
       placeholder={placeholder}
     />
   );
--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import Button from "../../components/ui/button/Button";
+import InputForm from "../../components/ui/input-form/InputForm";
 const Account = () => {
   return (
     <Layout>
-      <div className="container"></div>
+      <div className="container account-container">
+        <h1 className="main-title">My account</h1>
+
+        <form action="" className="create-account-form">
+          <div className="input-field">
+            <label htmlFor="name">Name</label>
+            <InputForm type="text" placeholder="Camila Yokoo" disabled={true} />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="email">Email address</label>
+            <InputForm
+              type="email"
+              placeholder="camilayokoo@gmail.com"
+              disabled={true}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <InputForm
+              type="password"
+              placeholder="*********"
+              disabled={true}
+            />
+          </div>
+        </form>
+
+        <Button text="Edit" outline={true} />
+      </div>
     </Layout>
   );
 };
