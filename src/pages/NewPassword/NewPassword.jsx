01fd59f326afb576287f03766226bbd1185b4eed
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-24 10:16:30 -0300

    new password page rebuild

--- src/pages/NewPassword/NewPassword.jsx
+++ src/pages/NewPassword/NewPassword.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Button from "Components/ui/button/Button";
 import InputForm from "Components/ui/input-form/InputForm";
-import "./NewPassword.css";
 import Logo from "Components/Logo/Logo";
-
+import Layout from 'Containers/Layout/Layout'
 const NewPassword = () => {
   return (
-    <>
-      <div className="container-signup">
+    <Layout>
         <Logo height="50px" classes={["logo-mobile"]} />
         <h1 className="main-title">Create a new password</h1>
         <p className="subtitle">Enter a new password for your account</p>
 
-        <form action="" className="sign-up-form">
+        <form action="">
           <div className="input-field">
             <label htmlFor="password">Password</label>
             <InputForm type="password" placeholder="*********" />
@@ -23,8 +21,7 @@ const NewPassword = () => {
           </div>
           <Button text="Confirm" />
         </form>
-      </div>
-    </>
+    </Layout>
   );
 };
 
