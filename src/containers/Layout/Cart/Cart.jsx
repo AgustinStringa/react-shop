9d2840dec089d1a2232943c1fee24a68eed8ed6a
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-24 21:10:21 -0300

    adding notification when carts have elements

--- src/containers/Layout/Cart/Cart.jsx
+++ src/containers/Layout/Cart/Cart.jsx
@@ -1,18 +1,21 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import IcoCart from "../../../assets/icons/icon_shopping_cart.svg";
 import { Link } from "react-router-dom";
 import MainMenuDesktop from "../MainMenuDesktop/MainMenuDesktop";
 import "Styles/Cart.css";
 import LateralWindow from 'Containers/LateralWindow'
 import ShoppingCart from 'Containers/ShoppingCart/ShoppingCart'
+import {ShopContext} from 'Context/useShop'
 const Cart = () => {
+  const {cart} = useContext(ShopContext);
   const [showCart, setShowCart] = useState(true);
   return (
     <>
     <div className="menu-cart">
       <MainMenuDesktop />
-      <button className="button-cart" onClick={()=> {setShowCart(!showCart)}}>
-        <img src={IcoCart} />
+      <button className={`button-cart ${cart.length > 1 && 'ico-cart-notify' }`}
+      onClick={()=> {setShowCart(!showCart)}}>
+        <img src={IcoCart}/>
       </button>
       {showCart && <LateralWindow onClose={() =>{ setShowCart(!showCart) }}>
         <ShoppingCart/>
